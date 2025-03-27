139f0c6f63252c716a443d35e15a4024e1ef9a0a
Jan Nanista <jan.jakub.nanista@gmail.com>
2025-03-26 17:01:35 -0700

    WIP

--- packages/contracts-bedrock/scripts/autogen/generate-script-interfaces/main.go
+++ packages/contracts-bedrock/scripts/autogen/generate-script-interfaces/main.go
@@ -7,12 +7,13 @@ import (
 	_ "embed"
 	"fmt"
 	"go/format"
+	"math/big"
 	"os"
 	"strings"
 	"text/template"
 
 	"github.com/ethereum-optimism/optimism/packages/contracts-bedrock/scripts/checks/common"
-	"github.com/ethereum/go-ethereum/accounts/abi"
+	_common "github.com/ethereum/go-ethereum/common"
 )
 
 //go:embed source.go.tpl
@@ -32,6 +33,15 @@ type templateMethod struct {
 type processedFile struct {
 	File string
 	Code string
+}
+
+type RunArgs struct {
+	SuperchainProxyAdminOwner  _common.Address `toml:"superchainProxyAdminOwner"`
+	ProtocolVersionsOwner      _common.Address `toml:"protocolVersionsOwner"`
+	Guardian                   _common.Address `toml:"guardian"`
+	Paused                     bool            `toml:"paused"`
+	RequiredProtocolVersion    *big.Int        `toml:"requiredProtocolVersion"`
+	RecommendedProtocolVersion *big.Int        `toml:"recommendedProtocolVersion"`
 }
 
 func main() {
@@ -70,9 +80,28 @@ func processFile(file string) (*processedFile, []error) {
 		fmt.Printf("Processing %s: Event %s: %v\n", file, name, event)
 	}
 
+	// var run = w3.MustNewFunc("run((address,address,address,bool,uint256,uint256) _input)", "(address,address,address,address,address) _output")
+
+	runArgs := RunArgs{
+		SuperchainProxyAdminOwner:  _common.BigToAddress(big.NewInt(1)),
+		ProtocolVersionsOwner:      _common.BigToAddress(big.NewInt(1)),
+		Guardian:                   _common.BigToAddress(big.NewInt(1)),
+		Paused:                     false,
+		RequiredProtocolVersion:    big.NewInt(1),
+		RecommendedProtocolVersion: big.NewInt(1),
+	}
+	packed, err := artifact.Abi.Parsed.Pack("run", runArgs)
+	if err != nil {
+		return nil, []error{fmt.Errorf("failed to pack %s: %w", file, err)}
+	}
+	fmt.Printf("Packed: %s", packed)
+
 	methods := []templateMethod{}
 	for name, method := range artifact.Abi.Parsed.Methods {
-		fmt.Printf("\tMethod: %s", method.String())
+		// if method.RawName == "emittedDeployOutputs" {
+		// 	packed, err := method.Inputs.Pack([]byte{})
+		// }
+
 		methodInputs := []string{}
 		for _, input := range method.Inputs {
 			methodInputs = append(methodInputs, fmt.Sprintf("%s %s", input.Type.String(), input.Name))
@@ -115,12 +144,31 @@ func processFile(file string) (*processedFile, []error) {
 	}, nil
 }
 
-func formatMethod(m abi.Method) string {
-	return fmt.Sprintf("%s()", m.RawName)
-}
+// func formatMethod(m abi.Method) string {
+// 	return fmt.Sprintf("%s()", m.RawName)
+// }
 
-func formatArgument(a abi.Argument) string {
-	if a.Indexed {
-		return fmt.Sprintf("%s indexed %s", a.Name)
-	}
-}
+// func formatArgument(a abi.Argument) string {
+// 	if a.Indexed {
+// 		return fmt.Sprintf("%s indexed %s", a.Name)
+// 	}
+// }
+
+//Unpack
+// def := fmt.Sprintf(`[{ "name" : "method", "type": "function", "outputs": %s}]`, test.def)
+// abi, err := JSON(strings.NewReader(def))
+// if err != nil {
+// 	t.Fatalf("invalid ABI definition %s: %v", def, err)
+// }
+// encb, err := hex.DecodeString(test.packed)
+// if err != nil {
+// 	t.Fatalf("invalid hex %s: %v", test.packed, err)
+// }
+// out, err := abi.Unpack("method", encb)
+// if err != nil {
+// 	t.Errorf("test %d (%v) failed: %v", i, test.def, err)
+// 	return
+// }
+// if !reflect.DeepEqual(test.unpacked, ConvertType(out[0], test.unpacked)) {
+// 	t.Errorf("test %d (%v) failed: expected %v, got %v", i, test.def, test.unpacked, out[0])
+// }
