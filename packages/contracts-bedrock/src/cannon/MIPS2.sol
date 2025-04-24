4c05241bc534ae5837007c32995fc62f3dd059b6
mbaxter <meredith@oplabs.co>
2025-04-24 13:27:25 -0400

    Undo MIPS2 version bump

--- packages/contracts-bedrock/src/cannon/MIPS2.sol
+++ packages/contracts-bedrock/src/cannon/MIPS2.sol
@@ -62,7 +62,7 @@ contract MIPS2 is ISemver {
 
     /// @notice The semantic version of the MIPS2 contract.
     /// @custom:semver 1.2.0
-    string public constant version = "1.2.0";
+    string public constant version = "1.1.0";
 
     /// @notice The preimage oracle contract.
     IPreimageOracle internal immutable ORACLE;
