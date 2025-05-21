41e488b0a02e8485ea05902b6ebcc46f953174aa
Park Changwan <pcw109550@gmail.com>
2025-05-21 18:26:39 +0900

    add comment in smoke test

--- op-acceptance-tests/tests/smoke/smoke_test.go
+++ op-acceptance-tests/tests/smoke/smoke_test.go
@@ -10,9 +10,10 @@ import (
 	"github.com/ethereum-optimism/optimism/op-service/txintent"
 	"github.com/ethereum-optimism/optimism/op-service/txintent/bindings"
 	"github.com/ethereum-optimism/optimism/op-service/txplan"
-	"github.com/ethereum/go-ethereum/common"
 )
 
+// TestWrapETH checks WETH interactions, testing both reading and writing on the chain.
+// This demonstrates the usage of DSL for contract bindings
 func TestWrapETH(gt *testing.T) {
 	t := devtest.SerialT(gt)
 	require := t.Require()
@@ -22,66 +23,67 @@ func TestWrapETH(gt *testing.T) {
 	bob := sys.Funder.NewFundedEOA(eth.ThousandEther)
 
 	client := sys.L2EL.Escape().EthClient()
-	wethAddr := common.HexToAddress("0x4200000000000000000000000000000000000006")
 
-	// dsl prep
-	factory := bindings.NewWETHCallFactory(bindings.WithTo(wethAddr), bindings.WithClient(client))
-	// can bind factory options later
+	// Contract binding preparation
+	// Embed EL client for reading the chain
+	factory := bindings.NewWETHCallFactory(bindings.WithClient(client))
+	// Use default WETH address
+	factory.WithDefaultAddr()
+	// We can bind other options such as tests later
 	factory.ApplyFactoryOptions(bindings.WithTest(t))
 
-	// initialize bindings from binding factory
+	// Initialize bindings from binding factory
 	weth := bindings.NewWETH(factory)
 
-	var err error
-	// alice and bob has zero WETH
+	// Fetch default WETH address from binding
+	wethAddr, _ := weth.To()
+
+	// Basic sanity check
 	require.NotEqual(alice.Address(), bob.Address())
 
+	// Alice and Bob has zero WETH
 	require.Equal(eth.ZeroWei, dsl.Read(weth.BalanceOf(alice.Address())))
 	require.Equal(eth.ZeroWei, dsl.Read(weth.BalanceOf(bob.Address())))
 
-	// alice wraps 1 WETH
-	alice.Transfer(wethAddr, eth.OneEther)
+	// Write: Alice wraps 1 WETH
+	alice.Transfer(*wethAddr, eth.OneEther)
 
-	// view
-	// alice has 1 WETH
+	// Read: Alice has 1 WETH
 	require.Equal(eth.OneEther, dsl.Read(weth.BalanceOf(alice.Address())))
-	// bob has 0 WETH.
+	// Read: Bob has 0 WETH
 	require.Equal(eth.ZeroWei, dsl.Read(weth.BalanceOf(bob.Address())))
 
-	// alice wraps 1 WETH again
-	alice.Transfer(wethAddr, eth.OneEther)
+	// Write: Alice wraps 1 WETH again
+	alice.Transfer(*wethAddr, eth.OneEther)
 
-	// view
-	// alice has 2 WETH
+	// Read: Alice has 2 WETH
 	require.Equal(eth.Ether(2), dsl.Read(weth.BalanceOf(alice.Address())))
-	// bob has 0 WETH.
+	// Read: Bob has 0 WETH
 	require.Equal(eth.ZeroWei, dsl.Read(weth.BalanceOf(bob.Address())))
 
-	// read(manual error handling)
-	// without address sender so failure
-	_, err = txintent.Read(weth.Transfer(bob.Address(), eth.OneEther), t.Ctx())
+	// Read not using the DSL. Therefore you need to manually error handle and also set context
+	_, err := txintent.Read(weth.Transfer(bob.Address(), eth.OneEther), t.Ctx())
+	// Will revert because tx.sender is not set
 	t.Require().Error(err)
-	// with address, msg.sender set
+	// Provide tx.sender using txplan
+	// Success beacuse tx.sender(Alice) has enough WETH
 	require.True(dsl.Read(weth.Transfer(bob.Address(), eth.OneEther), txplan.WithSender(alice.Address())))
 
-	// write
-	// alice sends bob 1 WETH
+	// Write: Alice sends Bob 1 WETH
 	dsl.Write(alice, weth.Transfer(bob.Address(), eth.OneEther))
 
-	// read
-	// alice has 1 WETH
+	// Read: Alice has 1 WETH
 	require.Equal(eth.OneEther, dsl.Read(weth.BalanceOf(alice.Address())))
-	// bob has 1 WETH.
+	// Read: Bob has 1 WETH
 	require.Equal(eth.OneEther, dsl.Read(weth.BalanceOf(bob.Address())))
 
-	// write(manual error handling)
-	// alice sends bob 1 WETH
+	// Write not using the DSL. Therefore you need to manually error handle
 	_, err = alice.Write(weth.Transfer(bob.Address(), eth.OneEther))
+	// Success beacuse tx.sender(Alice) has enough WETH
 	require.NoError(err)
 
-	// read
-	// alice has 0 WETH
+	// Read: Alice has 0 WETH
 	require.Equal(eth.ZeroWei, dsl.Read(weth.BalanceOf(alice.Address())))
-	// bob has 2 WETH.
+	// Read: Bob has 2 WETH
 	require.Equal(eth.Ether(2), dsl.Read(weth.BalanceOf(bob.Address())))
 }
