bd82c089af952216b721af2e7effde0b115d9146
Park Changwan <pcw109550@gmail.com>
2025-05-27 14:49:51 +0900

    port test

--- op-acceptance-tests/tests/smoke/smoke_test.go
+++ op-acceptance-tests/tests/smoke/smoke_test.go
@@ -1,6 +1,7 @@
 package smoke
 
 import (
+	"math/big"
 	"testing"
 
 	"github.com/ethereum-optimism/optimism/op-devstack/devtest"
@@ -84,4 +85,76 @@ func TestWrapETH(gt *testing.T) {
 	require.Equal(eth.ZeroWei, contract.Read(weth.BalanceOf(alice.Address())))
 	// Read: Bob has 2 WETH
 	require.Equal(eth.Ether(2), contract.Read(weth.BalanceOf(bob.Address())))
+}
+
+func TestWrapETH3(gt *testing.T) {
+	t := devtest.SerialT(gt)
+	require := t.Require()
+	sys := presets.NewMinimal(t)
+
+	alice := sys.Funder.NewFundedEOA(eth.ThousandEther)
+	bob := sys.Funder.NewFundedEOA(eth.ThousandEther)
+
+	client := sys.L2EL.Escape().EthClient()
+
+	// Contract binding preparation
+	// Embed EL client for reading the chain
+	factory := bindings.NewWETHCallFactory(
+		bindings.WithClient(client), bindings.WithTest(t),
+	)
+	// Use default WETH address
+	factory.WithDefaultAddr()
+
+	// Initialize bindings from binding factory
+	weth := bindings.NewWETH(factory)
+
+	// Fetch default WETH address from binding
+	wethAddr, _ := weth.To()
+
+	// Basic sanity check
+	require.NotEqual(alice.Address(), bob.Address())
+
+	// Alice and Bob has zero WETH
+	require.Zero(contract.Read3(weth.BalanceOf3(alice.Address())).Cmp(big.NewInt(0)))
+	require.Zero(contract.Read3(weth.BalanceOf3(bob.Address())).Cmp(big.NewInt(0)))
+
+	// Write: Alice wraps 1 WETH
+	alice.Transfer(*wethAddr, eth.OneEther)
+
+	// Read: Alice has 1 WETH
+	require.Zero(contract.Read3(weth.BalanceOf3(alice.Address())).Cmp(big.NewInt(1_000_000_000_000_000_000)))
+	// Read: Bob has 0 WETH
+	require.Zero(contract.Read3(weth.BalanceOf3(bob.Address())).Cmp(big.NewInt(0)))
+
+	// Write: Alice wraps 1 WETH again
+	alice.Transfer(*wethAddr, eth.OneEther)
+
+	// Read: Alice has 2 WETH
+	require.Zero(contract.Read3(weth.BalanceOf3(alice.Address())).Cmp(big.NewInt(2_000_000_000_000_000_000)))
+	// Read: Bob has 0 WETH
+	require.Zero(contract.Read3(weth.BalanceOf3(bob.Address())).Cmp(big.NewInt(0)))
+
+	// Read not using the DSL. Therefore you need to manually error handle and also set context
+	_, err := contractio.Read3(weth.Transfer3(bob.Address(), big.NewInt(1_000_000_000_000_000_000)), t.Ctx())
+	// Will revert because tx.sender is not set
+	require.Error(err)
+	// Provide tx.sender using txplan
+	// Success because tx.sender(Alice) has enough WETH
+	require.True(contract.Read3(weth.Transfer3(bob.Address(), big.NewInt(1_000_000_000_000_000_000)), txplan.WithSender(alice.Address())))
+
+	// Write: Alice sends Bob 1 WETH
+	contract.Write3(alice, weth.Transfer3(bob.Address(), big.NewInt(1_000_000_000_000_000_000)))
+
+	// Read: Alice has 1 WETH
+	require.Zero(contract.Read3(weth.BalanceOf3(alice.Address())).Cmp(big.NewInt(1_000_000_000_000_000_000)))
+	// Read: Bob has 1 WETH
+	require.Zero(contract.Read3(weth.BalanceOf3(bob.Address())).Cmp(big.NewInt(1_000_000_000_000_000_000)))
+
+	// Write: Alice sends Bob 1 WETH
+	contract.Write3(alice, weth.Transfer3(bob.Address(), big.NewInt(1_000_000_000_000_000_000)))
+
+	// Read: Alice has 0 WETH
+	require.Zero(contract.Read3(weth.BalanceOf3(alice.Address())).Cmp(big.NewInt(0)))
+	// Read: Bob has 2 WETH
+	require.Zero(contract.Read3(weth.BalanceOf3(bob.Address())).Cmp(big.NewInt(2_000_000_000_000_000_000)))
 }
