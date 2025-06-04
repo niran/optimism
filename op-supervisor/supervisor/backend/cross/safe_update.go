703f09b0c75a0ad3cb59a3fe9f1d2b5c3f97326e
axelKingsley <axel.kingsley@gmail.com>
2025-06-04 13:07:32 -0500

    update error message

--- op-supervisor/supervisor/backend/cross/safe_update.go
+++ op-supervisor/supervisor/backend/cross/safe_update.go
@@ -119,7 +119,7 @@ func scopedCrossSafeUpdate(h reads.Handle, logger log.Logger, chainID eth.ChainI
 		return candidate, fmt.Errorf("failed to determine dependencies of cross-safe candidate %s: %w", candidate.Derived, err)
 	}
 	if err := SufficientDataChecks(d, candidate.Source.ID(), hazards); err != nil {
-		return candidate, fmt.Errorf("failed to verify block %s in cross-safe frontier: %w", candidate.Derived, err)
+		return candidate, fmt.Errorf("insufficient data to verify block %s: %w", candidate.Derived, err)
 	}
 	if err := HazardSafeFrontierChecks(d, candidate.Source.ID(), hazards); err != nil {
 		return candidate, fmt.Errorf("failed to verify block %s in cross-safe frontier: %w", candidate.Derived, err)
