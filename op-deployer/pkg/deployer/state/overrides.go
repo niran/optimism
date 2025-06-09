6d8a666331d5c9eb72360c6ab15fafb8a868d838
Samuel Stokes <sam.adam.stokes@gmail.com>
2025-06-09 16:39:40 -0400

    buildValidOverrideKeys from existing structs

--- op-deployer/pkg/deployer/state/overrides.go
+++ op-deployer/pkg/deployer/state/overrides.go
@@ -4,68 +4,82 @@ package state
 
 import (
 	"fmt"
+	"reflect"
 	"sort"
 	"strings"
+
+	"github.com/ethereum-optimism/optimism/op-chain-ops/genesis"
 )
 
-// ValidOverrideKeys is a map of all keys that can be overridden in the deployment config
-var ValidOverrideKeys = map[string]struct{}{
-	// L2 Core parameters
-	"l2BlockTime":               {},
-	"l2GenesisBlockGasLimit":    {},
-	"finalizationPeriodSeconds": {},
-	"maxSequencerDrift":         {},
-	"sequencerWindowSize":       {},
+// BuildValidOverrideKeys dynamically extracts all field names from a DeployConfig struct
+func buildValidOverrideKeys() map[string]struct{} {
+	validKeys := make(map[string]struct{})
+	extractFieldNames(reflect.TypeOf(genesis.DeployConfig{}), "", validKeys)
+	extractFieldNames(reflect.TypeOf(SuperchainProofParams{}), "", validKeys)
+	extractFieldNames(reflect.TypeOf(ChainProofParams{}), "", validKeys)
+	return validKeys
+}
 
-	// Fee vault parameters
-	"l1FeeVaultMinimumWithdrawalAmount":        {},
-	"sequencerFeeVaultMinimumWithdrawalAmount": {},
-	"baseFeeVaultWithdrawalNetwork":            {},
-	"l1FeeVaultWithdrawalNetwork":              {},
-	"sequencerFeeVaultWithdrawalNetwork":       {},
+// extractFieldNames recursively extracts JSON field names from a struct type
+func extractFieldNames(t reflect.Type, prefix string, result map[string]struct{}) {
+	if t.Kind() == reflect.Ptr {
+		t = t.Elem()
+	}
 
-	// Gas parameters
-	"gasPriceOracleBaseFeeScalar":       {},
-	"gasPriceOracleBlobBaseFeeScalar":   {},
-	"gasPriceOracleOperatorFeeScalar":   {},
-	"gasPriceOracleOperatorFeeConstant": {},
+	if t.Kind() != reflect.Struct {
+		return
+	}
 
-	// Fault proof parameters
-	"useFaultProofs":                          {},
-	"faultGameWithdrawalDelay":                {},
-	"preimageOracleMinProposalSize":           {},
-	"preimageOracleChallengePeriod":           {},
-	"proofMaturityDelaySeconds":               {},
-	"disputeGameFinalityDelaySeconds":         {},
-	"mipsVersion":                             {},
-	"respectedGameType":                       {},
-	"faultGameAbsolutePrestate":               {},
-	"faultGameMaxDepth":                       {},
-	"faultGameSplitDepth":                     {},
-	"faultGameClockExtension":                 {},
-	"faultGameMaxClockDuration":               {},
-	"dangerouslyAllowCustomDisputeParameters": {},
+	for i := 0; i < t.NumField(); i++ {
+		field := t.Field(i)
 
-	// Hardfork timing parameters
-	"l2GenesisRegolithTimeOffset":           {},
-	"l2GenesisCanyonTimeOffset":             {},
-	"l2GenesisDeltaTimeOffset":              {},
-	"l2GenesisEcotoneTimeOffset":            {},
-	"l2GenesisFjordTimeOffset":              {},
-	"l2GenesisGraniteTimeOffset":            {},
-	"l2GenesisHoloceneTimeOffset":           {},
-	"l2GenesisIsthmusTimeOffset":            {},
-	"l2GenesisInteropTimeOffset":            {},
-	"l2GenesisJovianTimeOffset":             {},
-	"l2GenesisPectraBlobScheduleTimeOffset": {},
+		// Handle embedded structs (fields without names)
+		if field.Anonymous {
+			// Recursively extract fields from the embedded struct
+			extractFieldNames(field.Type, prefix, result)
+			continue
+		}
+
+		// Get JSON field name
+		jsonTag := field.Tag.Get("json")
+		if jsonTag == "" || jsonTag == "-" {
+			continue
+		}
+
+		fieldName := strings.Split(jsonTag, ",")[0]
+
+		// Add simple field name to support current usage pattern
+		result[fieldName] = struct{}{}
+
+		// Also add fully qualified name if we have a prefix
+		if prefix != "" {
+			fullPath := prefix + "." + fieldName
+			result[fullPath] = struct{}{}
+		}
+
+		// Recursively process nested structs
+		fieldType := field.Type
+		if fieldType.Kind() == reflect.Ptr {
+			fieldType = fieldType.Elem()
+		}
+
+		if fieldType.Kind() == reflect.Struct {
+			newPrefix := fieldName
+			if prefix != "" {
+				newPrefix = prefix + "." + fieldName
+			}
+			extractFieldNames(fieldType, newPrefix, result)
+		}
+	}
 }
 
 // ValidateOverrides checks if all keys in the overrides map are valid override keys
 func ValidateOverrides(overrides map[string]any) error {
-	var invalidKeys []string
+	validKeys := buildValidOverrideKeys()
 
+	var invalidKeys []string
 	for key := range overrides {
-		if _, ok := ValidOverrideKeys[key]; !ok {
+		if _, ok := validKeys[key]; !ok {
 			invalidKeys = append(invalidKeys, key)
 		}
 	}
@@ -74,14 +88,13 @@ func ValidateOverrides(overrides map[string]any) error {
 		sort.Strings(invalidKeys)
 
 		// Get valid keys for error message
-		validKeysList := make([]string, 0, len(ValidOverrideKeys))
-		for key := range ValidOverrideKeys {
+		validKeysList := make([]string, 0, len(validKeys))
+		for key := range validKeys {
 			validKeysList = append(validKeysList, key)
 		}
 		sort.Strings(validKeysList)
 
-		return fmt.Errorf("invalid override keys: %s\n\nValid keys are:\n%s\n",
-			strings.Join(invalidKeys, ", "), strings.Join(validKeysList, "\n"))
+		return fmt.Errorf("invalid override keys:\n%s", strings.Join(invalidKeys, ", "))
 	}
 
 	return nil
