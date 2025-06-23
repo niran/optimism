Necter-Network/necter-chain

op-deployer: Fix intent validation with pre-deployed OPCM (#15712)
* op-deployer: Fix intent validation with pre-deployed OPCM

Previously, users were always required to provide the `SuperchainRoles` in their intent. Now that the intent supports providing an OPCM address, this is no longer necessary since we can look those values up on chain. The lookup was implemented in #15623, however users still had to provide `SuperchainRoles` in their intent to avoid validation errors.

The new logic is as follows:

- `standard` intents **must** specify the correct, standard OPCM address for the specified L1 and contracts version. Anything else is an error.
- All other intent types must **either** specify `SuperchainRoles`, or an OPCM address. Specifying both or neither will result in an error.
- The deployment pipeline will always pull `SuperchainRoles` data from the chain if the OPCM address is specified. Previously, it would only do this if the OPCM address was specified _and_ the L1 locator was a tag.

* cr updates

op-deployer/pkg/deployer/state/intent_test.go
@@ -68,7 +68,7 @@ func TestValidateStandardValues(t *testing.T) {
 				addr := common.HexToAddress("0x9999")
 				intent.SuperchainConfigProxy = &addr
 			},
-			ErrNonStandardValue,
+			ErrIncompatibleValue,
 		},
 		{
 			"OPCMAddress",
@@ -77,6 +77,15 @@ func TestValidateStandardValues(t *testing.T) {
 				intent.OPCMAddress = &addr
 			},
 			ErrNonStandardValue,
+		},
+		{
+			"SuperchainRoles",
+			func(intent *Intent) {
+				intent.SuperchainRoles = &addresses.SuperchainRoles{
+					SuperchainGuardian: common.HexToAddress("0x9999"),
+				}
+			},
+			ErrIncompatibleValue,
 		},
 	}
 	for _, tt := range tests {
@@ -121,6 +130,50 @@ func TestValidateCustomValues(t *testing.T) {
 	setFeeAddresses(&intent)
 	err = intent.Check()
 	require.NoError(t, err)
+
+	tests := []struct {
+		name    string
+		mutator func(intent *Intent)
+		err     error
+	}{
+		{
+			"both OPCM and SuperchainRoles defined",
+			func(intent *Intent) {
+				addr := common.HexToAddress("0x9999")
+				intent.SuperchainRoles = &addresses.SuperchainRoles{
+					SuperchainGuardian: addr,
+				}
+				intent.OPCMAddress = &addr
+			},
+			ErrIncompatibleValue,
+		},
+		{
+			"neither OPCM or SuperchainRoles defined",
+			func(intent *Intent) {
+				intent.OPCMAddress = nil
+				intent.SuperchainRoles = nil
+			},
+			ErrIncompatibleValue,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			intent, err := NewIntentCustom(11155111, []common.Hash{common.HexToHash("0x336")})
+			require.NoError(t, err)
+
+			setSuperchainRoles(&intent)
+			setChainRoles(&intent)
+
+			setEip1559Params(&intent)
+			setFeeAddresses(&intent)
+
+			tt.mutator(&intent)
+
+			err = intent.Check()
+			require.Error(t, err)
+			require.ErrorIs(t, err, tt.err)
+		})
+	}
 }
 
 func setSuperchainRoles(intent *Intent) {

niran/optimism

op-deployer/pkg/deployer/state/intent_test.go
@@ -67,7 +67,7 @@ func TestValidateStandardValues(t *testing.T) {
 				addr := common.HexToAddress("0x9999")
 				intent.SuperchainConfigProxy = &addr
 			},
-			ErrNonStandardValue,
+			ErrIncompatibleValue,
 		},
 		{
 			"OPCMAddress",
@@ -76,6 +76,15 @@ func TestValidateStandardValues(t *testing.T) {
 				intent.OPCMAddress = &addr
 			},
 			ErrNonStandardValue,
+		},
+		{
+			"SuperchainRoles",
+			func(intent *Intent) {
+				intent.SuperchainRoles = &SuperchainRoles{
+					Guardian: common.HexToAddress("0x9999"),
+				}
+			},
+			ErrIncompatibleValue,
 		},
 	}
 	for _, tt := range tests {
@@ -120,6 +129,50 @@ func TestValidateCustomValues(t *testing.T) {
 	setFeeAddresses(&intent)
 	err = intent.Check()
 	require.NoError(t, err)
+
+	tests := []struct {
+		name    string
+		mutator func(intent *Intent)
+		err     error
+	}{
+		{
+			"both OPCM and SuperchainRoles defined",
+			func(intent *Intent) {
+				addr := common.HexToAddress("0x9999")
+				intent.SuperchainRoles = &SuperchainRoles{
+					Guardian: addr,
+				}
+				intent.OPCMAddress = &addr
+			},
+			ErrIncompatibleValue,
+		},
+		{
+			"neither OPCM or SuperchainRoles defined",
+			func(intent *Intent) {
+				intent.OPCMAddress = nil
+				intent.SuperchainRoles = nil
+			},
+			ErrIncompatibleValue,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			intent, err := NewIntentCustom(11155111, []common.Hash{common.HexToHash("0x336")})
+			require.NoError(t, err)
+
+			setSuperchainRoles(&intent)
+			setChainRoles(&intent)
+
+			setEip1559Params(&intent)
+			setFeeAddresses(&intent)
+
+			tt.mutator(&intent)
+
+			err = intent.Check()
+			require.Error(t, err)
+			require.ErrorIs(t, err, tt.err)
+		})
+	}
 }
 
 func setSuperchainRoles(intent *Intent) {